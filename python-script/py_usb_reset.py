6bd4a503d5b27755ac3600014bf9a38bd27c5929
Jose Damico <jose.damico@scicrop.com>
2021-08-06 20:57:54 -0300

    improved pyusb script

--- python-script/py_usb_reset.py
+++ python-script/py_usb_reset.py
@@ -1,4 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from usb.core import find as finddev
-dev = finddev(idVendor=0x0bda, idProduct=0x2838)
-dev.reset()
+import sys
+
+
+def main():
+    reset_usb_device(sys.argv[1], sys.argv[2])
+
+
+def reset_usb_device(id_vendor, id_product):
+    dev = finddev(idVendor=0x0bda, idProduct=0x2838)
+    dev.reset()
+
+
+if __name__ == "__main__":
+    main()
